d987bd658943615a1e1be2a850d39493e4d816f7
monodnb <monodnb@gmail.com>
2015-08-26 16:39:53 +0300

    meh

--- app/views/admin/admin.view.controller.js
+++ app/views/admin/admin.view.controller.js
@@ -2,10 +2,10 @@
     'use strict';
 
     angular.module('daksportsApp')
-        .controller('AdminCtrl', function($scope, $rootScope) {
+        .controller('AdminCtrl', function($scope, $rootScope, $mdDialog) {
             $rootScope.noNav = true;
             $scope.$on('$destroy', function() {
-              $rootScope.noNav = false;
+                $rootScope.noNav = false;
             });
         });
 
