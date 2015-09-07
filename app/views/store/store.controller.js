00f3ee6e5bb2742203202985735f31cd72c5cb26
monodnb <monodnb@gmail.com>
2015-09-07 14:40:35 +0300

    minor tweak to product grid display

--- app/views/store/store.controller.js
+++ app/views/store/store.controller.js
@@ -4,22 +4,7 @@
     angular.module('daksportsApp')
         .controller('StoreCtrl', function($rootScope, $scope, $state, BrandFilter, MainCatFilter, SubCatFilter) {
             $rootScope.state = $state.current.name;
-            $scope.results = [];
-            $rootScope.filtered = angular.copy($scope.results);
-
-            $scope.$watch(
-                // This function returns the value being watched. It is called for each turn of the $digest loop
-                function() {
-                    return $scope.results;
-                },
-                // This is the change listener, called when the value returned from the above function changes
-                function(newValue, oldValue) {
-                    if (newValue !== oldValue) {
-                        // Only increment the counter if the value changed
-                        $rootScope.filtered = angular.copy($scope.results);
-                    }
-                }
-            );
+            $scope.filtered = [];
 
             $scope.$on('$destroy', function() {
                 // Make sure that the interval is destroyed too
