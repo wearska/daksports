ade97d914b200f609acc21fa78ccc4a339c86b28
monodnb <monodnb@gmail.com>
2015-09-28 16:43:59 +0300

    starting product gallery

--- app/components/slider/slider.controller.js
+++ app/components/slider/slider.controller.js
@@ -2,8 +2,8 @@
   'use strict';
 
   angular.module('daksportsApp')
-    .controller('GdSlider', function($scope, $interval) {
-      // console.log("tada");
+    .controller('GdSliderCtrl', function($scope, $interval) {
+      console.log("tada");
     });
 
 })();
--- app/views/product/product.controller.js
+++ app/views/product/product.controller.js
@@ -2,7 +2,12 @@
     'use strict';
 
     angular.module('daksportsApp')
-        .controller('ProductCtrl', function($scope, $rootScope, $http, $parse, $filter, $timeout, $stateParams, productRes, ngCart, $state) {
+        .controller('ProductCtrl', function($scope, $rootScope, $http, $parse, $filter, $timeout, $stateParams, $state, $mdDialog, productRes, ngCart) {
+
+            //-----------------------------------------
+            //  INITIAL STATE
+            //-----------------------------------------
+            $scope.slides = [];
 
             // Make appbar transparent
             $rootScope.transparentAppbar = true;
@@ -17,45 +22,43 @@
                 $rootScope.transparentAppbar = true;
                 $rootScope.mainScrolled = false;
             });
-            $scope.heroes = [
-                {
-                    file: 'assets/img/heroes/baschet.jpg',
-                    position: 'right'
-                },
-                {
-                    file: 'assets/img/heroes/tenis.jpg',
-                    position: 'left'
-                },
-            ]
+            $scope.heroes = [{
+                file: 'assets/img/heroes/baschet.jpg',
+                position: 'right'
+            }, {
+                file: 'assets/img/heroes/tenis.jpg',
+                position: 'left'
+            }, ]
 
             var rnd = $filter('randomize')($scope.heroes.length);
             console.log(rnd);
-            $scope.hero = $scope.heroes[rnd-1];
+            $scope.hero = $scope.heroes[rnd - 1];
 
             // Initial state
-            if(!$rootScope.products){
-                $scope.$on('products:filled', function(){
+            if (!$rootScope.products) {
+                $scope.$on('products:filled', function() {
                     $scope.product = $filter('filter')($rootScope.products, function(d) {
                         return d.code === $stateParams.productCode;
                     })[0];
-                    // var str = $scope.product.description;
-                    // str = str.split(/\n/);
-                    // $timeout(function(){
-                    //     $scope.product.description = str;
-                    // }, 3000)
-                    // console.log($scope.product.description);
+
+                    $scope.slides.push($scope.product.file1);
+                    $scope.slides.push($scope.product.file2);
+                    $scope.slides.push($scope.product.file3);
+                    $scope.slides.push($scope.product.file4);
+                    $scope.slides.push($scope.product.file5);
+                    $scope.sliderLength = $scope.slides.length;
                 });
-            }else if($rootScope.products){
+            } else if ($rootScope.products) {
                 $scope.product = $filter('filter')($rootScope.products, function(d) {
                     return d.code === $stateParams.productCode;
                 })[0];
-                // var str = $scope.product.description;
-                // str = str.split(/\r?\n/);
-                // $timeout(function(){
-                //     console.log(str);
-                //     $scope.product.description = str;
-                // }, 3000)
-                // console.log($scope.product.description);
+
+                $scope.slides.push($scope.product.file1);
+                $scope.slides.push($scope.product.file2);
+                $scope.slides.push($scope.product.file3);
+                $scope.slides.push($scope.product.file4);
+                $scope.slides.push($scope.product.file5);
+                $scope.sliderLength = $scope.slides.length;
             }
 
 
@@ -75,36 +78,6 @@
                 $scope.mainView = file;
             }
 
-            // function setTypes(item) {
-            //     item.added = new Date(item.added);
-            //     item.inv = parseFloat(item.inv);
-            //     item.price = parseFloat(item.price).toFixed(2);
-            //     item.promo = parseFloat(item.promo);
-            //     item.published = parseFloat(item.published);
-            //     item.promo_price = parseFloat(item.promo_price).toFixed(2);
-            //     item.promo_stock = parseFloat(item.promo_stock);
-            //     if (item.promo && item.promo_price) {
-            //         item.old_price = item.price;
-            //         item.new_price = item.promo_price;
-            //     } else {
-            //         item.old_price = 0;
-            //         item.new_price = item.price;
-            //     };
-            //     (item.tags) ? item.tags = item.tags.split(','): item.tags = [];
-            //     (item.colours) ? item.colours = item.colours.split(','): item.colours = [];
-            //     (angular.isDate(item.promo_end)) ? item.promo_end = item.promo_end: item.promo_end = new Date(item.promo_end);
-            // }
-            //
-            // // ADD TO FAV
-            //
-            // function setFavourite(id) {
-            //     var product = $filter('filter')($rootScope.products, function(d) {
-            //         return d.id === id;
-            //     })[0];
-            //     console.log(product);
-            //     product.favourite = true;
-            // };
-
             $scope.postFav = function(productid) {
                 var data = {
                     userid: $rootScope.userData.uid,
@@ -120,49 +93,28 @@
                     });
             }
 
-            // productRes.get($stateParams.productId)
-            //     .then(function(response) {
-            //         var i = 0;
-            //         $scope.product = response.data[0];
-            //         setTypes($scope.product);
-            //         // SET SLIDE LENGTH
-            //         angular.forEach($scope.product, function(value, key) {
-            //             if (key.indexOf("file") !== -1 && value.indexOf("placeholder") == -1) {
-            //                 i++;
-            //                 $scope.sliderLength = i;
-            //                 value = value.replace(/(\.[\w\d_-]+)$/i, '_medium$1');
-            //                 var model = $parse(key);
-            //                 model.assign($scope.files, value);
-            //             }
-            //             // CHECK IF FAV
-            //             $timeout(function() {
-            //                 angular.forEach($rootScope.userData.favs, function(value, key) {
-            //                     if ($scope.product.code == value) {
-            //                         $scope.product.favourite = true;
-            //                     }
-            //                 });
-            //             }, 1000);
-            //
-            //         });
-            //         $scope.product.sizes = [
-            //             "XS",
-            //             "S",
-            //             "M",
-            //             "L",
-            //             "XL"
-            //         ];
-            //         $scope.product.colours = [
-            //             "Rosu",
-            //             "Galben",
-            //             "Albastru"
-            //         ];
-            //         $scope.product.rating = 50;
-            //     }).catch(function(error) {
-            //         return error;
-            //     });
-
             $scope.addToCart = function() {
                 ngCart.addItem($scope.product.code, $scope.product.name, parseFloat($scope.product.price), parseInt($scope.order.quantity), $scope.product);
+            };
+
+
+            //-----------------------------------------
+            //  PRODUCT GALLERY
+            //-----------------------------------------
+
+            $scope.showGallery = function(ev) {
+                $mdDialog.show({
+                        controller: 'GdSliderCtrl',
+                        templateUrl: 'app/views/product/product.gallery.inc.html',
+                        parent: angular.element(document.body),
+                        targetEvent: ev,
+                        clickOutsideToClose: true
+                    })
+                    .then(function(answer) {
+                        $scope.status = 'You said the information was "' + answer + '".';
+                    }, function() {
+                        $scope.status = 'You cancelled the dialog.';
+                    });
             };
 
 
