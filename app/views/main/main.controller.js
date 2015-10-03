24739fb1b8eadaffeffd9f754fb4908cd5dc41a5
monodnb <monodnb@gmail.com>
2015-10-03 23:23:20 +0300

    a few fixes before color thief

--- app/views/main/main.controller.js
+++ app/views/main/main.controller.js
@@ -2,7 +2,7 @@
     'use strict';
 
     angular.module('daksportsApp')
-        .controller('MainCtrl', ['$scope', '$http', '$location', function($scope, $http, $location) {
+        .controller('MainCtrl', function($scope, $rootScope, $http, $filter, $location) {
             $scope.slides = [
                 "/uploads/slider/slide_adidas.jpg",
                 "/uploads/slider/slide_nike.jpg",
@@ -10,6 +10,20 @@
                 "/uploads/slider/slide_reebok.jpg"
             ];
             $scope.sliderLength = $scope.slides.length;
-        }]);
+            
+            //------------------------------
+            // Carousel filters
+            //------------------------------
+            
+            var getShoes = function(){
+                var shoes = $filter('kindFilter')($rootScope.products, ['Incaltaminte']);
+                $scope.shoes = $filter('orderBy')(shoes, 'added', false);
+            }
+            getShoes();
+            $scope.$on('ratings:filled', function(){
+                getShoes();
+            });
+            
+        });
 
 })();
